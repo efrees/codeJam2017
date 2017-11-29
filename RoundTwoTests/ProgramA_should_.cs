6406f98b2a697fffd8b37135ac12e5ea18fc138a
Eric Frees <efrees@atlasrfid.com>
2017-11-29 09:17:53 -0600

    Pushing round 2 tests files.

---
+++ RoundTwoTests/ProgramA_should_.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using RoundTwo;
+
+namespace RoundTwoTests
+{
+    public class ProgramA_should_
+    {
+        [Test]
+        public void solve_case_correctly()
+        {
+            var gList = new[] { 4, 1, 1, 1, 1, 1 };
+            var p = 2;
+
+            var answer = ProgramA.ProcessTestCase(gList, p);
+
+            Assert.AreEqual(4, answer);
+        }
+
+        [Test]
+        public void solve_3_case_correctly()
+        {
+            var gList = new[] { 3, 1, 1, 1, 1, 1 };
+            var p = 3;
+
+            var answer = ProgramA.ProcessTestCase(gList, p);
+
+            Assert.AreEqual(3, answer);
+        }
+
+        [Test]
+        public void solve_4_case_correctly()
+        {
+            var p = 3;
+            var gList = new[] { 4, 1, 1, 1, 1, 1, 2, 2 };
+
+            var answer = ProgramA.ProcessTestCase(gList, p);
+            Assert.AreEqual(4, answer);
+
+            gList = new[] { 4, 1, 3, 1, 1, 1, 2, 2 };
+
+            answer = ProgramA.ProcessTestCase(gList, p);
+            Assert.AreEqual(4, answer);
+
+            gList = new[] { 4, 1, 3, 1, 1, 1, 1, 2, 2 };
+
+            answer = ProgramA.ProcessTestCase(gList, p);
+            Assert.AreEqual(5, answer);
+
+            gList = new[] { 4, 1, 3, 1, 1, 1, 1, 2, 2, 2 };
+
+            answer = ProgramA.ProcessTestCase(gList, p);
+            Assert.AreEqual(5, answer);
+
+        }
+    }
+}
---
+++ RoundTwoTests/ProgramB_should_.cs
@@ -0,0 +1,129 @@
+﻿using NUnit.Framework;
+using RoundTwo;
+
+namespace RoundTwoTests
+{
+    internal class ProgramB_should_
+    {
+        [Test]
+        public void solve_customer_constrained_case()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 1, Seat = 2},
+                new Ticket {Customer = 1, Seat = 1},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("3 0", answer);
+        }
+
+        [Test]
+        public void solve_seat_constrained_case()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 2, Seat = 1},
+                new Ticket {Customer = 1, Seat = 1},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("3 0", answer);
+        }
+
+        [Test]
+        public void solve_overlap_case()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 2, Seat = 1},
+                new Ticket {Customer = 1, Seat = 2},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("2 0", answer);
+        }
+
+        [Test]
+        public void solve_promotion_case()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 2},
+                new Ticket {Customer = 2, Seat = 2},
+                new Ticket {Customer = 1, Seat = 2},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("2 1", answer);
+        }
+
+        [Test]
+        public void solve_example_4_correctly()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 2, Seat = 3},
+                new Ticket {Customer = 1, Seat = 2},
+                new Ticket {Customer = 3, Seat = 3},
+                new Ticket {Customer = 1, Seat = 3},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("2 1", answer);
+        }
+
+        [Test]
+        public void avoid_early_promotion()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 1, Seat = 2},
+                new Ticket {Customer = 1, Seat = 3},
+                new Ticket {Customer = 2, Seat = 2},
+                new Ticket {Customer = 2, Seat = 2},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("3 0", answer);
+        }
+
+        [Test]
+        public void choose_match_that_avoids_later_promotion()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 1, Seat = 2},
+                new Ticket {Customer = 1, Seat = 3},
+                new Ticket {Customer = 2, Seat = 1},
+                new Ticket {Customer = 2, Seat = 2},
+                new Ticket {Customer = 2, Seat = 3},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("3 0", answer);
+        }
+
+        [Test]
+        public void choose_match_that_avoids_later_promotion2()
+        {
+            var tickets = new[]
+            {
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 1, Seat = 1},
+                new Ticket {Customer = 1, Seat = 2},
+                new Ticket {Customer = 2, Seat = 3},
+                new Ticket {Customer = 2, Seat = 3},
+                new Ticket {Customer = 2, Seat = 2},
+            };
+
+            var answer = ProgramB.ProcessTestCase(tickets, 2);
+            Assert.AreEqual("3 0", answer);
+        }
+    }
+}
